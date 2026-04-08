57d904358d4b8bef83874e3f55908b58991f435d
Milosz Fraczek <255586@edu.p.lodz.pl>
2026-04-08 14:59:59 +0200

    modyfikacja klasy DefaultCountingOutRhymer - zastosowanie akcesorow i hermetyzacji

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -11,7 +11,7 @@ public class DefaultCountingOutRhymer {
 
     private int[] numbers = new int[CAPACITY];
 
-    public int total = -1;
+    private int total = -1;
 
     public void countIn(int in) {
         if (!isFull())
@@ -38,4 +38,7 @@ public class DefaultCountingOutRhymer {
         return numbers[total--];
     }
 
+    public int getTotal() {
+        return total;
+    }
 }
