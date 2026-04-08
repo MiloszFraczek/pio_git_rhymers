df064ec8521d8844f39032b65eeda02b2f666561
Milosz Fraczek <255586@edu.p.lodz.pl>
2026-04-08 15:24:59 +0200

    modyfikacja klasy IntLinkedList - zastosowanie akcesorow i hermetyzacji

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,10 +2,10 @@ package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
 
-    final int ERROR = -1;
+    private final int ERROR = -1;
 
-    Node last;
-    int i;
+    private Node last;
+    private int i;
 
     public void push(int i) {
         if (last == null)
